0bd307f5e11420a76bb21e230a8be1f7c109bafa
BIGBen99 <bvef@hotmail.com>
2022-04-24 14:08:22 +0200

    init

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -2,17 +2,13 @@
 namespace BIGBen\MyWebSite\Controller;
 
 require_once('framework/Controller.php');
-require_once('model/PostManager.php');
 
 class HomeController extends \BIGBen\MyWebSite\Framework\Controller {
-    private $postManager;
 
     public function __construct() {
-        $this->postManager = new \BIGBen\MyWebSite\Model\PostManager();
     }
 
     public function index() {
-        $posts = $this->postManager->getPosts();
-        $this->generateView(array('posts' => $posts));
+        $this->generateView();
     }
 }
